37c9fd92723dd8f064d952a42cecb01aa5e9fbd8
oregonpillow <oregonpillow@gmail.com>
2021-07-03 18:40:23 +0200

    updates

--- Django Features and Libraries/autos/views.py
+++ Django Features and Libraries/autos/views.py
@@ -26,65 +26,6 @@ class MakeView(LoginRequiredMixin, View):
         return render(request, 'autos/make_list.html', ctx)
 
 
-'''
-class MakeCreate(LoginRequiredMixin, View):
-    template = 'autos/make_form.html'
-    success_url = reverse_lazy('autos:all')
-
-    def get(self, request):
-        form = MakeForm()
-        ctx = {'form': form}
-        return render(request, self.template, ctx)
-
-    def post(self, request):
-        form = MakeForm(request.POST)
-        if not form.is_valid():
-            ctx = {'form': form}
-            return render(request, self.template, ctx)
-
-        make = form.save()
-        return redirect(self.success_url)
-
-
-class MakeUpdate(LoginRequiredMixin, View):
-    model = Make
-    success_url = reverse_lazy('autos:all')
-    template = 'autos/make_form.html'
-
-    def get(self, request, pk):
-        make = get_object_or_404(self.model, pk=pk)
-        form = MakeForm(instance=make)
-        ctx = {'form': form}
-        return render(request, self.template, ctx)
-
-    def post(self, request, pk):
-        make = get_object_or_404(self.model, pk=pk)
-        form = MakeForm(request.POST, instance=make)
-        if not form.is_valid():
-            ctx = {'form': form}
-            return render(request, self.template, ctx)
-
-        form.save()
-        return redirect(self.success_url)
-
-
-class MakeDelete(LoginRequiredMixin, View):
-    model = Make
-    success_url = reverse_lazy('autos:all')
-    template = 'autos/make_confirm_delete.html'
-
-    def get(self, request, pk):
-        make = get_object_or_404(self.model, pk=pk)
-        form = MakeForm(instance=make)
-        ctx = {'make': make}
-        return render(request, self.template, ctx)
-
-    def post(self, request, pk):
-        make = get_object_or_404(self.model, pk=pk)
-        make.delete()
-        return redirect(self.success_url)
-'''
-
 class MakeCreate(LoginRequiredMixin, CreateView):
     model = Make
     fields = '__all__'
